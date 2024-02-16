d43348b45cf2d3f757c82555a7c9855d8bebc07f
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-02-16 13:36:08 -0500

    Add rough-work folder

---
+++ rough-work/parser-functions.js
@@ -0,0 +1,5 @@
+function cleanStr(str) {
+    //strip str of all whitespace
+    return str.replaceAll(/\s/g, "");
+}
+console.log(cleanStr("as (*^&TGuyv ui gy9vu79 uy     79yf 97y   fgv97 y 97 y"));
