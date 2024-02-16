696e742d2460653dea4d283a8baaba6080a63c47
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-02-16 13:45:04 -0500

    Add tokenizeChar fn

--- rough-work/parser-functions.js
+++ rough-work/parser-functions.js
@@ -2,4 +2,11 @@ function cleanStr(str) {
     //strip str of all whitespace
     return str.replaceAll(/\s/g, "");
 }
-console.log(cleanStr("as (*^&TGuyv ui gy9vu79 uy     79yf 97y   fgv97 y 97 y"));
+
+function tokenizeChar(type, val, input, current) {
+    //tokenize single char
+    if (type === input[current]) {
+        return [1, {type, val}];
+    }
+    return [0, null];
+}
