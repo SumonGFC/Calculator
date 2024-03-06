e4a1cefd0586d5eaf58e3bed33218db5cce8f3a5
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-03-06 06:10:09 -0500

    Write initial test case for lexer

--- translator/lexer.js
+++ translator/lexer.js
@@ -1,16 +1,41 @@
 
 
 
-const string1 = "1 + 2 * 3 / ( 4 - 5 ) ^ - 6";
+const string1 = "1.123 + 0.2 * .3 / ( 450 - 5 ) ^ ( - 6 )";
+const expectedOutput = [
+    {type: "FLOAT", value: 1.123},
+    {type: "BINOP", value: "ADD"},
+    {type: "FLOAT", value: 0.2},
+    {type: "BINOP", value: "MULTIPLY"},
+    {type: "FLOAT", value: 0.3},
+    {type: "BINOP", value: "DIVIDE"},
+    {type: "LPAREN", value: "("},
+    {type: "FLOAT", value: 450.0},
+    {type: "BINOP", value: "SUBTRACT"},
+    {type: "FLOAT", value: 5.0},
+    {type: "RPAREN", value: ")"},
+    {type: "BINOP", value: "EXPONENT"},
+    {type: "LPAREN", value: "("},
+    {type: "UNARYOP", value: "-"},
+    {type: "FLOAT", value: 6.0},
+    {type: "RPAREN", value: ")"},
+]
 
-function scan(str) {
-    //remove whitespace
-    return str.replaceAll(/\s/g, "");
+class Lexer {
+    constructor(_expr) {
+        this.expr = _expr;
+        this.cursor = 0;
+    }
+
+    scan(str) {
+        //remove whitespace
+        return str.replaceAll(/\s/g, "");
+    }
+
+    tokenize() {
+
+    }
 }
 
-
-function test() {
-    console.log(scan(string1));
-}
-
-test();
+let test = new Lexer;
+console.log(test.tokenize());
