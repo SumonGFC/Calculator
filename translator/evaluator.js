ee27b786096b465afe2d0ff5809feacbf434b186
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-03-11 17:17:20 -0400

    Add evaluate method to return error messages

--- translator/evaluator.js
+++ translator/evaluator.js
@@ -1,44 +1,58 @@
-
-/* TODO: Make sure to change the code to accomodate errors in the actual
- * calculator -- i.e. handle error message properly so we can pass the error
- * message for invalid exponentiation to the calculator html.
- */
-
 class Evaluator {
     constructor() {
+        this.validExpr = true;
         this.errorMessage = "";
     }
 
-    evaluate(subTree) {
+    #traverse(subTree) {
         if (!isNaN(subTree.leaf)) {
             return subTree.leaf;
         } else {
+            const left = this.#traverse(subTree.left);
+            const right = this.#traverse(subTree.right);
             switch(subTree.operation) {
                 case "+":
-                    return this.evaluate(subTree.left) + this.evaluate(subTree.right);
+                    return left + right;
                 case "-":
-                    return this.evaluate(subTree.left) - this.evaluate(subTree.right);
+                    return left - right;
                 case "*":
-                    return this.evaluate(subTree.left) * this.evaluate(subTree.right);
+                    return left*right;
                 case "/":
-                    return this.evaluate(subTree.left) / this.evaluate(subTree.right);
+                    if (right !== 0 ) {
+                        return left/right;
+                    } else {
+                        this.errorMessage = "Division by 0 is unsupported.";
+                        this.validExpr = false;
+                        throw new Error("Divide by 0");
+                    }
                 case "^":
-                    const left = this.evaluate(subTree.left);
-                    const right = this.evaluate(subTree.right);
                     const tmp = left**right;
                     if (isNaN(tmp)) {
-                        this.errorMessage += "Error: It seems we could not " +
-                        " evaluate an exponent. Complex number arithmetic is " +
-                        "unsupported at this time.\n";
+                        this.errorMessage = "Error: It seems we could not " +
+                        "evaluate exponentiation. Complex number arithmetic " +
+                        "is unsupported at this time.";
+                        this.validExpr = false;
                         throw new Error("Couldn't evaluate exponentiation");
                     } else {
                         return tmp;
                     }
                 case "u":
-                    return (-1)*(this.evaluate(subTree.left));
+                    return (-1)*(left);
                 default:
                     throw new Error("Error: Invalid operator in AST.")
             }
+        }
+    }
+
+    evaluate(AST) {
+        const result = this.#traverse(AST);
+        if (this.validExpr) {
+            return result;
+        } else {
+            const msg = this.errorMessage;
+            this.errorMessage = "";
+            this.validExpr = true;
+            return msg;
         }
     }
 }
